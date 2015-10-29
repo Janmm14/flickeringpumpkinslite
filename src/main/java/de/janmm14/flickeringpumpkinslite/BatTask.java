6b5d487e2b17b9a475653279ac57bd360b41b264
Janmm14 <computerjanimaus@yahoo.de>
2015-10-29 18:08:40 +0100

    Spawn multiple (3) bats, play sound only with chance of 25% instead of 100%

--- src/main/java/de/janmm14/flickeringpumpkinslite/BatTask.java
+++ src/main/java/de/janmm14/flickeringpumpkinslite/BatTask.java
@@ -8,6 +8,8 @@ import org.bukkit.entity.Bat;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -18,48 +20,65 @@ public class BatTask extends BukkitRunnable {
 	private final Random random = new Random();
 	private final FlickeringPumpkinsLite plugin;
 	private final Location pumpkinLocation;
-	private Bat bat;
+	private List<Bat> bats;
 	private int count;
 
 	@Override
 	public void run() {
 		if (count > 20) {
-			if (bat != null) {
-				bat.setNoDamageTicks(0);
-				bat.remove();
+			if (bats != null) {
+				for (Bat bat : bats) {
+					bat.setNoDamageTicks(0);
+					bat.remove();
+				}
 			}
 			tryCancel();
 			return;
 		}
-		if (bat == null) {
-			bat = pumpkinLocation.getWorld().spawn(pumpkinLocation.clone().add(.5, 1.5, .5), Bat.class);
-			bat.setMaxHealth(2048.0);
-			bat.setHealth(2048.0);
-			bat.setNoDamageTicks(Integer.MAX_VALUE);
-			bat.setRemoveWhenFarAway(true);
-			bat.setAwake(true);
-			playScarySound(pumpkinLocation);
+		if (bats == null) {
+			bats = new ArrayList<>();
+			for (int i = 3; i > 0; i--) {
+				Bat bat = pumpkinLocation.getWorld().spawn(pumpkinLocation.clone().add(.5, 1.5, .5), Bat.class);
+				bat.setMaxHealth(2048.0);
+				bat.setHealth(2048.0);
+				bat.setNoDamageTicks(Integer.MAX_VALUE);
+				bat.setRemoveWhenFarAway(true);
+				bat.setAwake(true);
+				bats.add(bat);
+			}
+			if (random.nextInt(4) == 0) { //chance 25% to play sound, 75% of bat spawns, no sound is played
+				playScarySound(pumpkinLocation); //only once, not per bat
+			}
 			return;
 		}
-		if (bat.isDead() || !bat.isValid()) {
+		if (bats.isEmpty()) {
 			tryCancel();
 			return;
+		}
+		Iterator<Bat> iterator = bats.iterator();
+		while (iterator.hasNext()) {
+			Bat bat = iterator.next();
+			if (bat.isDead() || !bat.isValid()) {
+				iterator.remove();
+			}
 		}
 
 		count++;
 
-		if (random.nextInt(10) != 0) { //chance 10% to continue - 90% return here
-			return;
-		}
+		for (Bat bat : bats) {
+			if (random.nextInt(10) != 0) { //chance 10% to continue - 90% return here
+				return;
+			}
 
-		for (int i = 10; i > 0; i--) {
-			final Location pLoc = bat.getLocation().clone()
-				.add(random.nextDouble() - random.nextDouble(),
-					random.nextDouble() - random.nextDouble(),
-					random.nextDouble() - random.nextDouble());
+			for (int i = 10; i > 0; i--) {
+				final Location pLoc = bat.getLocation().clone()
+					.add(random.nextDouble() - random.nextDouble(),
+						random.nextDouble() - random.nextDouble(),
+						random.nextDouble() - random.nextDouble());
 
-			FlickeringPumpkinsLiteUpdater.sendParticle(pLoc, Color.BLACK);
-			FlickeringPumpkinsLiteUpdater.sendParticle(pLoc, Color.RED);
+				FlickeringPumpkinsLiteUpdater.sendParticle(pLoc, Color.BLACK);
+				FlickeringPumpkinsLiteUpdater.sendParticle(pLoc, Color.RED);
+			}
 		}
 	}
 
@@ -67,9 +86,9 @@ public class BatTask extends BukkitRunnable {
 		playScarySound(loc, FlickeringPumpkinsLiteUpdater.getNearbyPlayers(loc, SOUND_DISTANCE_SQUARED));
 	}
 
-	private void playScarySound(Location loc, List<Player> players) {
+	private void playScarySound(Location loc, List<Player> players) { //TODO make sound playing configurable
 		for (Player plr : players) {
-			plr.playSound(loc, Sound.AMBIENCE_CAVE, random.nextFloat() * 2, 0); //floats: volume - pitch
+			plr.playSound(loc, Sound.AMBIENCE_CAVE, 1 + random.nextFloat(), 0); //floats: volume - pitch
 		}
 	}
 
